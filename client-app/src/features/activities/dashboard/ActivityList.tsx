2633e807f31fb37364951f5d04057e87b0ebb352
abutkys <abutkys4@gmail.com>
2021-03-15 15:56:35 +0200

    End of section 8

--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,20 +1,22 @@
 import { observer } from "mobx-react-lite";
 import React, { SyntheticEvent, useState } from "react";
-import { Button, Item, Label,  Segment } from "semantic-ui-react";
+import { Link } from "react-router-dom";
+import { Button, Item, Label, Segment } from "semantic-ui-react";
 import { useStore } from "../../../app/stores/store";
 
 export default observer(function ActivityList() {
-
   const { activityStore } = useStore();
-  const {deleteActivity, activitiesByDate, loading} = activityStore;
-  const [target, setTarget] = useState('');
+  const { deleteActivity, activitiesByDate, loading } = activityStore;
+  const [target, setTarget] = useState("");
 
-  function handleActivityDelete(e: SyntheticEvent<HTMLButtonElement>, id: string) {
+  function handleActivityDelete(
+    e: SyntheticEvent<HTMLButtonElement>,
+    id: string
+  ) {
     setTarget(e.currentTarget.name);
     deleteActivity(id);
   }
 
-  
   return (
     <Segment>
       <Item.Group divided>
@@ -31,7 +33,8 @@ export default observer(function ActivityList() {
               </Item.Description>
               <Item.Extra>
                 <Button
-                  onClick={() => activityStore.selectActivity(activity.id)}
+                  as={Link}
+                  to={`/activities/${activity.id}`}
                   floated="right"
                   content="View"
                   color="teal"
@@ -52,4 +55,4 @@ export default observer(function ActivityList() {
       </Item.Group>
     </Segment>
   );
-})
+});
