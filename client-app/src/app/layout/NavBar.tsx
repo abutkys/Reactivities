6d8d4bacd796e508f03d5a27726493df9f894fab
abutkys <abutkys4@gmail.com>
2021-03-28 01:19:22 +0200

    End of section 10

--- client-app/src/app/layout/NavBar.tsx
+++ client-app/src/app/layout/NavBar.tsx
@@ -1,23 +1,31 @@
-import { NavLink } from 'react-router-dom';
-import { Button, Container, Menu } from 'semantic-ui-react';
-import { useStore } from '../stores/store';
-
+import { NavLink } from "react-router-dom";
+import { Button, Container, Menu } from "semantic-ui-react";
+import { useStore } from "../stores/store";
 
 export default function NavBar() {
-
   const { activityStore } = useStore();
-    return (
-      <Menu inverted fixed="top">
-        <Container>
-          <Menu.Item as={NavLink} to='/' exact header>
-                    <img src="/assets/logo.png" alt="photo" style={{marginRight: '10px'}} />
-            Reactivities
-          </Menu.Item>
-          <Menu.Item as={NavLink} to='/activities' name="Activities" />
-          <Menu.Item>
-            <Button as={NavLink} to='/createActivity' positive content="Create Activity" />
-          </Menu.Item>
-        </Container>
-      </Menu>
-    );
+  return (
+    <Menu inverted fixed="top">
+      <Container>
+        <Menu.Item as={NavLink} to="/" exact header>
+          <img
+            src="/assets/logo.png"
+            alt="photo"
+            style={{ marginRight: "10px" }}
+          />
+          Reactivities
+        </Menu.Item>
+        <Menu.Item as={NavLink} to="/activities" name="Activities" />
+        <Menu.Item as={NavLink} to="/errors" name="Errors" />
+        <Menu.Item>
+          <Button
+            as={NavLink}
+            to="/createActivity"
+            positive
+            content="Create Activity"
+          />
+        </Menu.Item>
+      </Container>
+    </Menu>
+  );
 }
